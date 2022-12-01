f57965ca74331ec217a9c49e5fc4a51907410773
aborger <borger.aaron@gmail.com>
2022-11-30 22:30:53 -0700

    Teensy communicates with ros

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
-
 #include <Arduino.h>
 #include <comm.h> 
 
+Packet packet;
+
+void setup(void)
+{
+	pinMode(13, OUTPUT);
+	packet.led = false;
+} 
+
+void loop(void)
+{
+	delay(100);
+	
+	get_data(&packet);
+	digitalWrite(13, packet.led);
+
+	packet.cmd += 1;
+
+	write_data(&packet);
+}
+
 int main(void)
 {
-
-	pinMode(13, OUTPUT);
-	Packet packet;
-	packet.led = false;
 	while (1) {
-
-		delay(100);
-		
-		get_data(&packet);
-		digitalWrite(13, packet.led);
-
-		packet.cmd += 1;
-
-		write_data(&packet);
+		loop();
 	}
 
 }
